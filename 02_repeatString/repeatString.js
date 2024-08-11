6ac7aa69ed2d12024245981bf36e15cc5cf4cc19
orphick <mmahdavisalimi@gmail.com>
2024-08-11 17:41:52 +0330

    solve the problem creating the code, changing the test file so itll test all the situations

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    let con = "";
+    if (num > 0) {
+        for (let i = 0; i < num; i++) {
+            con += string;
+        }
+    } else if (num === 0) {
+        return "";
+    } else {
+        return "ERROR";
+    }
+    return con;
 };
 
 // Do not edit below this line
