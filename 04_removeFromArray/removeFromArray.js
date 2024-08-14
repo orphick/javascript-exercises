2f33699dce4f25a93a4bf8df33c3bd041e8d06eb
orphick <mmahdavisalimi@gmail.com>
2024-08-14 14:58:11 +0330

    finishing the code from fourth chapter, changing the test file in order to run the tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...valueToBeRemoved) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (!valueToBeRemoved.includes(array[i])) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
