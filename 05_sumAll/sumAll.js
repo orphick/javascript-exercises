4eb8476f72ce0d336c666826f67db4b233fa6730
orphick <mmahdavisalimi@gmail.com>
2024-08-14 17:33:39 +0330

    modify code so itll past all the tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,13 +1,27 @@
 const sumAll = function (firstNumber, secondNumber) {
     let myArray = [];
     let sumOfArray = null;
-    for (let i = firstNumber; i <= secondNumber; i++) {
-        myArray.push(i);
+    if (firstNumber < 1 || secondNumber < 1) {
+        return 'ERROR'
+    } else if (!Number.isInteger(firstNumber) || !Number.isInteger(secondNumber)) {
+        return 'ERROR'
+    } else if (secondNumber < firstNumber) {
+        for (let i = secondNumber; i <= firstNumber; i++) {
+            myArray.push(i);
+        }
+        for (let i = 0; i < myArray.length; i++) {
+            sumOfArray += myArray[i];
+        }
+        return sumOfArray;
+    } else {
+        for (let i = firstNumber; i <= secondNumber; i++) {
+            myArray.push(i);
+        }
+        for (let i = 0; i < myArray.length; i++) {
+            sumOfArray += myArray[i];
+        }
+        return sumOfArray;
     }
-    for (let i = 0; i < myArray.length; i++) {
-        sumOfArray += myArray[i];
-    }
-    return sumOfArray;
 };
 
 // Do not edit below this line
