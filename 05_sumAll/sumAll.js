1b14641b92db6ba360cc43d99771bfd15a011f4e
orphick <mmahdavisalimi@gmail.com>
2024-08-14 17:05:39 +0330

    finishing the code so itll work on the first two test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,13 @@
 const sumAll = function (firstNumber, secondNumber) {
     let myArray = [];
+    let sumOfArray = null;
     for (let i = firstNumber; i <= secondNumber; i++) {
         myArray.push(i);
     }
+    for (let i = 0; i < myArray.length; i++) {
+        sumOfArray += myArray[i];
+    }
+    return sumOfArray;
 };
 
 // Do not edit below this line
