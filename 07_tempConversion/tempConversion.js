107fd00dcfb02774f9cc3c34d6ea3e999631a7ac
orphick <mmahdavisalimi@gmail.com>
2024-08-18 11:52:21 +0330

    write the code for fixing the decimal number problem

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
 const convertToCelsius = function (degree) {
   let celsiusDegree = (degree - 32) * (5 / 9);
-  return celsiusDegree;
+  let roundCelsius = Math.round(celsiusDegree * 10) / 10;
+  return roundCelsius;
 };
 
 const convertToFahrenheit = function (degree) {
   let fahrenheitDegree = (degree * 1.8) + 32;
-  return fahrenheitDegree;
+  let roundFahrenheit = Math.round(fahrenheitDegree * 10) / 10;
+  return roundFahrenheit;
 };
 
 // Do not edit below this line
--- playground.js
+++
@@ -1,7 +0,0 @@
-let celsiusDegree = 25;
-let convertedFahrenheitDegree = (celsiusDegree * 1.8) + 32;
-console.log(convertedFahrenheitDegree);
-
-let fahrenheitDegree = 77;
-let convertedCelsiusDegree = (fahrenheitDegree - 32) * (5 / 9);
-console.log(convertedCelsiusDegree);
