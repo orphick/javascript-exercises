df1cd1209109046b201d3e7b22ad5088eeef8496
orphick <mmahdavisalimi@gmail.com>
2024-08-18 11:17:42 +0330

    rewriting the code again, pass all the tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
 const leapYears = function (years) {
-
+    if (years % 400 === 0) {
+        return true;
+    } else if (years % 100 === 0) {
+        return false;
+    } else if (years % 4 === 0 && years % 100 !== 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
