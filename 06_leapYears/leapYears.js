5a2e7bbd69ae5718ff0731a573689f90efc8fedb
orphick <mmahdavisalimi@gmail.com>
2024-08-18 10:48:22 +0330

    finish the code so itll pass the first test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function (years) {
+    let leapYear = null;
+    if (years > 100 && leapYear / 4 == 0) {
+        leapYear = years % 100;
+        return true;
+    } else if (years < 100 && years % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
