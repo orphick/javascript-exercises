3a677384a7ff10826058b14d297ed65587010c52
orphick <mmahdavisalimi@gmail.com>
2024-08-18 10:58:46 +0330

    removing everything and starting from beggining, change the tests so itll test every scenrios

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,13 +1,5 @@
 const leapYears = function (years) {
-    let leapYear = null;
-    if (years > 100 && leapYear / 4 == 0) {
-        leapYear = years % 100;
-        return true;
-    } else if (years < 100 && years % 4 == 0) {
-        return true;
-    } else {
-        return false;
-    }
+
 };
 
 // Do not edit below this line
